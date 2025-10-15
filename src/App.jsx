362c1dfd21fe61b55223b9286253d057161896c9
Akari <rebavalosaduviri@crackthecode.la>
2025-10-15 17:57:14 -0500

    App.jsx, otra vez

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,25 @@
-import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+import Catalogo from "./pages/Catalogo";
+import QuienesSomos from "./pages/QuienesSomos";
+import MisionVision from "./pages/MisionVision";
+import Contacto from "./pages/Contacto";
 
 function App() {
-
   return (
-    <>
-    <div>
-      <body>
-        <header>
-         <div class="logo">
-          <span>Sembrando Conocimientos</span>
-          </div>
-          <nav>
-          <ul>
-             <li><a href="/index.html">Inicio</a></li>
-             <li><a href="/assets/html/quienes-somos.html">Nosotras</a></li>
-             <li><a href="/assets/html/mision.html">Misión y Visión</a></li>
-             <li><a href="/assets/html/foro.html">Foro</a></li>
-             <li><a href="/assets/html/testimonios.html">Testimonios</a></li>
-             <li><a href="/assets/html/formulario.html">Contactanos</a></li>
-          </ul>
-          </nav>
-       </header>
-
-   <section class="hero">
-     <div class="contenedor">
-       <h1>Bienvenid@s a <span>Sembrando Conocimientos</span></h1>
-       <p>Un espacio donde cultivamos ideas, compartimos aprendizajes y crecemos junt@s.</p>
-     </div>
-  </section>
-
-  <footer>
-    <p>© 2025 Sembrando Conocimientos | Todos los derechos reservados</p>
-  </footer>
-
-</body>
-
-    </div>
-    </>
-  )
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/catalogo" element={<Catalogo />} />
+          <Route path="/quienes-somos" element={<QuienesSomos />} />
+          <Route path="/mision-vision" element={<MisionVision />} />
+          <Route path="/contacto" element={<Contacto />} />
+        </Routes>
+      </Layout>
+    </Router>
+  );
 }
 
-export default App
+export default App;
