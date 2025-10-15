38691385580180fe17a554fec5d0236bbc5effae
Akari <rebavalosaduviri@crackthecode.la>
2025-10-15 18:44:32 -0500

    App.jsx otra vez

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
+import Layout from "./Components/Layout";
 import Home from "./pages/Home";
 import Catalogo from "./pages/Catalogo";
 import QuienesSomos from "./pages/QuienesSomos";
