c0c610aa9dbde19762d227df1377f30e64f9a019
Akari <rebavalosaduviri@crackthecode.la>
2025-10-15 17:20:17 -0500

    Inicio

---
+++ src/pages/Home.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+<body>
+  <header>
+    <div class="logo">
+      <span>Sembrando Conocimientos</span>
+    </div>
+    <nav>
+      <ul>
+        <li><a href="/index.html">Inicio</a></li>
+        <li><a href="/assets/html/quienes-somos.html">Nosotras</a></li>
+        <li><a href="/assets/html/mision.html">Misión y Visión</a></li>
+        <li><a href="/assets/html/foro.html">Foro</a></li>
+        <li><a href="/assets/html/testimonios.html">Testimonios</a></li>
+        <li><a href="/assets/html/formulario.html">Contactanos</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <section class="hero">
+    <div class="contenedor">
+      <h1>Bienvenid@s a <span>Sembrando Conocimientos</span></h1>
+      <p>Un espacio donde cultivamos ideas, compartimos aprendizajes y crecemos junt@s.</p>
+    </div>
+  </section>
+
+  <footer>
+    <p>© 2025 Sembrando Conocimientos | Todos los derechos reservados</p>
+  </footer>
+
+</body>
+    </div>
+  )
+}
+
+export default Home
