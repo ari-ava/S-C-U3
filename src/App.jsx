512c2aea42b4ef6b8de6b91d396c88db85f37dce
Akari <rebavalosaduviri@crackthecode.la>
2025-10-15 17:25:28 -0500

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <body>
+        <header>
+         <div class="logo">
+          <span>Sembrando Conocimientos</span>
+          </div>
+          <nav>
+          <ul>
+             <li><a href="/index.html">Inicio</a></li>
+             <li><a href="/assets/html/quienes-somos.html">Nosotras</a></li>
+             <li><a href="/assets/html/mision.html">Misión y Visión</a></li>
+             <li><a href="/assets/html/foro.html">Foro</a></li>
+             <li><a href="/assets/html/testimonios.html">Testimonios</a></li>
+             <li><a href="/assets/html/formulario.html">Contactanos</a></li>
+          </ul>
+          </nav>
+       </header>
+
+   <section class="hero">
+     <div class="contenedor">
+       <h1>Bienvenid@s a <span>Sembrando Conocimientos</span></h1>
+       <p>Un espacio donde cultivamos ideas, compartimos aprendizajes y crecemos junt@s.</p>
+     </div>
+  </section>
+
+  <footer>
+    <p>© 2025 Sembrando Conocimientos | Todos los derechos reservados</p>
+  </footer>
+
+</body>
+
+    </div>
     </>
   )
 }
