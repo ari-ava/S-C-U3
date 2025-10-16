5c48ffeb57c7c9ffbc861a529fdfff13f3384544
Akari <rebavalosaduviri@crackthecode.la>
2025-10-15 22:22:07 -0500

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout.jsx";
-
+import Layout from "./Components/Layout.jsx";
 import Home from "./pages/Home.jsx";
 import Nosotras from "./pages/Nosotras.jsx";
 import MisionVision from "./pages/MisionVision.jsx";
