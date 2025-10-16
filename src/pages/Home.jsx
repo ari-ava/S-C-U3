885273da5d738fec6576416551454eb7a2ebc0fc
Akari <rebavalosaduviri@crackthecode.la>
2025-10-15 22:06:20 -0500

    home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+  return (
+    <div className="p-8 text-center">
+      <h1 className="text-2xl font-bold text-orange-700">
+        Sobre Nosotras 🌻
+      </h1>
+    </div>
+  );
+};
+
+export default Home;
